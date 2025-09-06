28b494b4ecfc38bf2d6f6cda94790da35dcc3db4
Ian Moffett <ian@osmora.org>
2025-09-06 18:22:54 -0400

    build: Add initial build script
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+build_binutils() {
+	cd build-binutils
+	../dirty/binutils-2.45/configure --target=x86_64-pc-ethos --prefix=$(pwd);
+	make
+	make install
+
+	cd ../
+	echo "[?] binutils is in build-binutils/"
+}
+
+if [[ -d build-binutils/ ]]
+then
+	echo "build-binutils/ already exists, rebuilding..."
+	build_binutils
+	exit 0
+fi
+
+# Clean everything up
+echo "Cleaning up..."
+rm -rf clean/clean build/ dirty/
+
+# Extract the clean sources
+echo "Extracting binutils..."
+cd clean/
+tar -xzvf binutils-2.45.gz
+cd ../
+
+# Patch binutils
+echo "Patching binutils..."
+mkdir -p dirty/
+cp -r clean/clean/* dirty/
+patch -s -p0 < patches/binutils-2.45.patch
+rm -rf clean/clean/
+
+# Build binutils
+echo "Building binutils"
+mkdir -p build-binutils/
+build_binutils
