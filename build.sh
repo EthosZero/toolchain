59abbfec1e7c543b5e7723544260fb1f8bb2c34c
Ian Moffett <ian@osmora.org>
2025-09-07 00:46:32 -0400

    scripts: refactor builds and patch gcc
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- build.sh
+++ build.sh
@@ -3,6 +3,38 @@ set -e
 
 NCORES=$(nproc)
 
+patch_binutils() {
+    # Extract the clean sources
+    echo "Extracting binutils..."
+    cd clean/
+    tar -xzvf binutils-2.45.gz
+    cd ../
+
+    # Patch binutils
+    echo "Patching binutils..."
+    mkdir -p dirty/
+    cp -r clean/clean/* dirty/
+    patch -s -p0 < patches/binutils-2.45.patch
+    rm -rf clean/clean/
+}
+
+patch_gcc() {
+    rm -rf dirty/
+    echo "Extracting gcc..."
+    cd clean/
+    tar -xvf gcc-13.4.0.tar.xz
+    cd ../
+
+    # Patch it
+    echo "Patching gcc..."
+    mkdir -p dirty/
+    cp -r clean/gcc-13.4.0/ dirty/
+    patch -s -p0 < patches/gcc-13.4.0.patch
+
+    rm -rf dirty/gcc-13.4.0/ dirty/
+    mv clean/gcc-13.4.0 gcc-patched
+}
+
 build_binutils() {
     mkdir -p build-binutils/
     cd build-binutils
@@ -14,30 +46,15 @@ build_binutils() {
     echo "[?] binutils is in build-binutils/"
 }
 
-if [[ -d build-binutils/ ]]
-then
-    echo "build-binutils/ already exists, rebuilding..."
-    build_binutils
-    exit 0
-fi
-
 # Clean everything up
 echo "Cleaning up..."
 rm -rf clean/clean build/ dirty/
 
-# Extract the clean sources
-echo "Extracting binutils..."
-cd clean/
-tar -xzvf binutils-2.45.gz
-cd ../
-
-# Patch binutils
-echo "Patching binutils..."
-mkdir -p dirty/
-cp -r clean/clean/* dirty/
-patch -s -p0 < patches/binutils-2.45.patch
-rm -rf clean/clean/
+patch_binutils
 
 # Build binutils
 echo "Building binutils"
 build_binutils
+
+patch_gcc
+echo "gcc requires manual build in gcc-patched/"
